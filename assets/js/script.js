80daf732c38580bab780af64458732ca4f3f972f
spereira15 <105558705+spereira15@users.noreply.github.com>
2023-05-02 23:13:51 -0400

    added loop to displayed stored user input on each item

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,42 @@
 $(function () {
-  var getDate = dayjs()
-  var currentHour = getDate.format('H')
+  var getDate = dayjs();
+  var currentHour = getDate.format("H");
 
-  $("#currentDay").text(getDate.format('dddd, MMMM D'))
+  $("#currentDay").text(getDate.format("dddd, MMMM D"));
 
-  console.log(currentHour)
+  console.log(currentHour);
 
   for (let i = 9; i <= 17; i++) {
-    var hourBlockDiv = $("#"+i);
+    var hourBlockDiv = $("#" + i);
 
-    console.log(hourBlockDiv)
+    console.log(hourBlockDiv);
     if (i < currentHour) {
-      hourBlockDiv.addClass('past')
-      hourBlockDiv.removeClass('present')
-      hourBlockDiv.removeClass('present')
+      hourBlockDiv.addClass("past");
+      hourBlockDiv.removeClass("present");
+      hourBlockDiv.removeClass("present");
     } else if (i === currentHour) {
-      hourBlockDiv.removeClass('past')
-      hourBlockDiv.addClass('present')
-      hourBlockDiv.removeClass('future')
+      hourBlockDiv.removeClass("past");
+      hourBlockDiv.addClass("present");
+      hourBlockDiv.removeClass("future");
     } else {
-      hourBlockDiv.removeClass('past')
-      hourBlockDiv.removeClass('present')
-      hourBlockDiv.addClass('future')
+      hourBlockDiv.removeClass("past");
+      hourBlockDiv.removeClass("present");
+      hourBlockDiv.addClass("future");
     }
-  } 
-  
-  $(".saveBtn").on("click", function() {
+  }
+
+  $(".saveBtn").on("click", function () {
     var description = $(this).siblings(".description").val();
     var timeBlockId = $(this).parent().attr("id");
 
     localStorage.setItem(timeBlockId, description);
   });
-  
 
+  for (let i = 9; i <= 17; i++) {
+    var hourBlockDiv = $("#" + i);
+    var savedDescription = localStorage.getItem(i);
+    if (savedDescription) {
+      hourBlockDiv.children(".description").val(savedDescription);
+    }
+  }
 });
